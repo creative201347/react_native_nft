2043ee1ccdd9ed327ce4f2d02f76c2637d7f50ea
Nabin Dhami <creative201347@gmail.com>
2022-04-02 13:55:40 +0545

    details section navigation readmore functionality in desc

--- components/index.js
+++ components/index.js
@@ -1,3 +1,5 @@
 export { default as FocusedStattusBar } from "./FocusedStattusBar";
 export { default as HomeHeader } from "./HomeHeader";
 export { default as NFTCard } from "./NFTCard";
+export { default as DetailsBid } from "./DetailsBid";
+export { default as DetailsDesc } from "./DetailsDesc";
